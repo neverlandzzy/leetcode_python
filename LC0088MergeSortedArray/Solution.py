cb5adfad582d92417da191510d4db9279e282a24
neverlandzzy <neverland@neverlands-mbp.lan>
2025-02-24 22:36:22 -0800

    update on 02/24

---
+++ LC0088MergeSortedArray/Solution.py
@@ -0,0 +1,102 @@
+"""
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-10^9 <= nums1[i], nums2[j] <= 10^9
+"""
+
+
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        i = m - 1
+        j = n - 1
+        index = m + n - 1
+
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[index] = nums1[i]
+                i -= 1
+            else:
+                nums1[index] = nums2[j]
+                j -= 1
+
+            index -= 1
+
+        while i >= 0:
+            nums1[index] = nums1[i]
+            i -= 1
+            index -= 1
+
+        while j >= 0:
+            nums1[index] = nums2[j]
+            j -= 1
+            index -= 1
+
+
+def main():
+    nums11 = [1, 2, 3, 0, 0, 0]
+    m1 = 3
+    nums12 = [2, 5, 6]
+    n1 = 3
+
+    nums21 = [1]
+    m2 = 1
+    nums22 = []
+    n2 = 0
+
+    nums31 = [0]
+    m3 = 0
+    nums32 = [1]
+    n3 = 1
+
+    solution = Solution()
+    solution.merge(nums11, m1, nums12, n1)
+    solution.merge(nums21, m2, nums22, n2)
+    solution.merge(nums31, m3, nums32, n3)
+
+    print("Merged array nums11:", nums11)
+    print("Merged array nums21", nums21)
+    print("Merged array nums31:", nums31)
+
+
+if __name__ == '__main__':
+    main()
